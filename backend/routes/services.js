ffd1268aacf4a4527200ff56cae8552b364cab28
jmalle25 <jmalle25@uh.cougarnet.edu>
2023-04-27 12:19:11 -0500

    services routes added to backend-tested w/ postman

---
+++ backend/routes/services.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+
+const org = process.env.ORG
+
+// importing data model schemas
+const { services } = require('../models/models')
+
+// GET services for org
+router.get('/', (req, res, next) => {
+    services
+        .find({ orgs: org }, (error, data) => {
+            if (error) {
+                return next(error)
+            } else {
+                return res.json(data)
+            }
+        })
+        .sort({ updatedAt: -1 })
+        .limit(10)
+})
+
+// POST new service
+router.post('/', (req, res, next) => {
+    const newService = req.body
+    newService.orgs = [org]
+    services.create(newService, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+// PUT update Service
+router.put('/update/:id', (req, res, next) => {
+    services.findByIdAndUpdate(req.params.id, req.body, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+module.exports = router
